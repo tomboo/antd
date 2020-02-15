e670b5f29d21692257f848fdbfa03b64dee097f3
Tom Booster <tom.booster@gmail.com>
2020-02-15 11:55:58 -0800

    Refactor

--- src/index.js
+++ src/index.js
@@ -9,16 +9,20 @@ function App() {
   const [ date, setDate ] = useState(null);
 
   function handleChange(date) {
-    message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
+    message.info(fdate(date));
     setDate(date);
   };
+
+  function fdate(date) {
+    return `Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`;
+  }
 
   return (
     <div style={{ width: 400, margin: '100px auto' }}>
       <DatePicker onChange={handleChange} />
       <div style={{ marginTop: 20 }}>
         <Alert
-          message={`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`}
+          message={fdate(date)}
           type="success"
         />
       </div>
