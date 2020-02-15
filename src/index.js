caa8eaf554f5fc7327cf34e2a0a600fe6198310b
Tom Booster <tom.booster@gmail.com>
2020-02-15 11:44:37 -0800

    Convert to useState hook

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { DatePicker, message, Alert } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
 
-class App extends React.Component {
-  state = {
-    date: null,
-  };
+function App() {
+  const [ date, setDate ] = useState(null);
 
-  handleChange = date => {
+  function handleChange(date) {
     message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
-    this.setState({ date });
+    setDate(date);
   };
 
-  render() {
-    const { date } = this.state;
-    return (
-      <div style={{ width: 400, margin: '100px auto' }}>
-        <DatePicker onChange={this.handleChange} />
-        <div style={{ marginTop: 20 }}>
-          <Alert message={`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`} type="success" />
-        </div>
+  return (
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 20 }}>
+        <Alert
+          message={`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`}
+          type="success"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
